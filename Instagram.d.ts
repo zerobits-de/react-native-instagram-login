f36e8b543ed766fd94029016575197b843ccfb20
olliiiver <olliiiver@example.com>
2024-08-17 23:41:30 +0200

    more types

--- Instagram.d.ts
+++ Instagram.d.ts
@@ -3,26 +3,35 @@ declare module 'react-native-instagram-login' {
   import { StyleProp, ViewStyle, ImageStyle } from 'react-native';
   
   interface InstagramLoginProps {
-      ref: React.RefObject<any>
-      appId: string;
-      appSecret?: string;
-      redirectUrl: string;
-      scopes?: string[];
-      onLoginSuccess?: (data?: { access_token: string; user_id: number }) => void
-      onLoginFailure?: (error: string) => void
-      modalVisible?: boolean;
-      responseType?: string;
-      containerStyle?: StyleProp<ViewStyle>;
-      wrapperStyle?: StyleProp<ViewStyle>;
-      closeStyle?: StyleProp<ViewStyle>;
-      renderClose?: () => React.ReactNode;
-      onClose?: () => void;
-      styles?: {
-          webView?: StyleProp<ViewStyle>;
-      };
-      language?: string;
-      incognito?: boolean;
+    ref: React.RefObject<any>
+    appId: string;
+    appSecret?: string;
+    redirectUrl: string;
+    scopes?: string[];
+    onLoginSuccess?: (data?: { access_token: string; user_id: number }) => void
+    onLoginFailure?: (error: string) => void
+    modalVisible?: boolean;
+    responseType?: string;
+    containerStyle?: StyleProp<ViewStyle>;
+    wrapperStyle?: StyleProp<ViewStyle>;
+    closeStyle?: StyleProp<ViewStyle>;
+    renderClose?: () => React.ReactNode;
+    onClose?: () => void;
+    styles?: {
+      webView?: StyleProp<ViewStyle>;
+    };
+    language?: string;
+    incognito?: boolean;
+  }
+
+  interface InstagramState {
+    modalVisible: boolean;
+    key: number;
   }
   
-  export default class InstagramLogin extends React.Component<InstagramLoginProps> {}
+  export default class InstagramLogin extends React.Component<InstagramLoginProps, InstagramState> {
+    constructor(props: InstagramLoginProps);
+    show(): void;
+    hide(): void;
+  }
 }
